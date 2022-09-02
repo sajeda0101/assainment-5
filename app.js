4dad12d019d27194ca41a952bd3146492a265a3b
unknown <sajeda0101>
2022-09-02 10:06:40 -0700

    news category complete

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// categorty section
 const categoryLoad=()=>{
     fetch(` https://openapi.programming-hero.com/api/news/categories`)
     .then(res=>res.json())
@@ -13,9 +14,53 @@ const displayCategory=(categorys)=>{
 
         const catogoryDiv=document.createElement('div');
         catogoryDiv.innerHTML=`
-        <h5 class="bg-secondary text-white p-3">${category.category_name}</h5>
+        <h5 class="bg-secondary text-white p-3" onclick="newsLoad()">${category.category_name}</h5>
         `
         categoryContainer.appendChild(catogoryDiv)
         })
 }
 categoryLoad()
+
+// news section
+
+const newsLoad=()=>{
+    fetch(` https://openapi.programming-hero.com/api/news/category/01`)
+    .then(res=>res.json())
+    .then(data=>newsDisplay(data.data))
+
+}
+const newsDisplay=(categoryNews)=>{
+    console.log(categoryNews)
+    const newsContainer=document.getElementById('news-container');
+    categoryNews.forEach(categoryNew=>{
+    
+    const newsDiv=document.createElement('div')
+    newsDiv.classList.add('col')
+    newsDiv.innerHTML=`
+    <div class="card h-100">
+    <img src="${categoryNew.thumbnail_url}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${categoryNew.title}</h5>
+      <p class="card-text text-truncate">${categoryNew.details}</p>
+    </div>
+    <div class="d-flex justify-content-evenly">
+    <img src="${categoryNew.author.img}" class="w-25 rounded-circle ms-2 mb-2">
+    <div class="ms-2 mt-4">
+    <h5>${categoryNew.author.name}</h5>
+    <p>${categoryNew.author. published_date}<p>
+    </div>
+   
+    <div class="ms-5">
+    <p><i class="fa-regular fa-arrow-right"></i></p>
+    <h3>${categoryNew.rating.number}M</h3>
+    </div> </div>
+  </div>
+    
+    `
+    newsContainer.appendChild(newsDiv)
+
+
+  })
+
+}
+newsLoad()
