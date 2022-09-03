7041b949a6e071e73ec1872181583c638ce1016a
unknown <sajeda0101>
2022-09-02 20:57:40 -0700

    blog html complete

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // categorty section
 const categoryLoad=()=>{
-    fetch(` https://openapi.programming-hero.com/api/news/categories`)
+    fetch(`  https://openapi.programming-hero.com/api/news/categories`)
     .then(res=>res.json())
     .then(data=>displayCategory(data.data.news_category))
 }
@@ -8,13 +8,13 @@ const categoryLoad=()=>{
 const displayCategory=(categorys)=>{
 
     // category container
+    console.log(categorys) 
     const categoryContainer=document.getElementById('category-container');
     categorys.forEach(category=>{
-        // console.log(category) 
 
         const catogoryDiv=document.createElement('div');
         catogoryDiv.innerHTML=`
-        <h5 class="bg-secondary text-white p-3" onclick="newsLoad()">${category.category_name}</h5>
+        <button class="btn" onclick="newsLoad('${category.category_id}')">${category.category_name}<button>
         `
         categoryContainer.appendChild(catogoryDiv)
         })
@@ -23,8 +23,8 @@ categoryLoad()
 
 // news section
 
-const newsLoad=()=>{
-    fetch(` https://openapi.programming-hero.com/api/news/category/01`)
+const newsLoad=(category_id)=>{
+    fetch(` https://openapi.programming-hero.com/api/news/category/0${category_id}`)
     .then(res=>res.json())
     .then(data=>newsDisplay(data.data))
 
@@ -43,17 +43,18 @@ const newsDisplay=(categoryNews)=>{
       <h5 class="card-title">${categoryNew.title}</h5>
       <p class="card-text text-truncate">${categoryNew.details}</p>
     </div>
-    <div class="d-flex justify-content-evenly">
+    <div class="d-flex">
     <img src="${categoryNew.author.img}" class="w-25 rounded-circle ms-2 mb-2">
-    <div class="ms-2 mt-4">
+    <div class=" ms-2 mt-5">
     <h5>${categoryNew.author.name}</h5>
     <p>${categoryNew.author. published_date}<p>
     </div>
    
-    <div class="ms-5">
-    <p><i class="fa-regular fa-arrow-right"></i></p>
-    <h3>${categoryNew.rating.number}M</h3>
-    </div> </div>
+   <div class="mt-5 d-flex ">
+   <h3 class="ms-5">${categoryNew.rating.number}M</h3>
+   <buton class="btn btn-primary h-25  p-4 fs-6 ms-5" data-bs-toggle="modal" data-bs-target="#newsModal">Details <span class="ms-3"><i class="fa-solid fa-arrow-right"></i></span></button>
+  </div>
+  </div>
   </div>
     
     `
@@ -63,4 +64,4 @@ const newsDisplay=(categoryNews)=>{
   })
 
 }
-newsLoad()
+// newsLoad()
